792ec1b5cc3491276c13b03488987d567363690b
Jeremiah Onojah <jayonojah@gmail.com>
2023-10-03 05:36:55 +0100

    Added More Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import './globals.css';
+
 import Sidebar from '@/components/Sidebar';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 
@@ -18,7 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
