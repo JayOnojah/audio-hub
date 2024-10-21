3cad12b530983b73ff01a64305fa658fcdff44e0
Jeremiah <jayonojah@gmail.com>
2024-10-21 17:02:44 +0100

    Code Refactor

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import AuthModal from '@/components/AuthModal';
-import UploadModal from '@/components/UploadModal';
-import SubscribeModal from '@/components/SubscribeModal';
-import { ProductWithPrice } from '@/types';
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+import SubscribeModal from "@/components/SubscribeModal";
+import { ProductWithPrice } from "@/types";
 
 interface ModalProviderProps {
   products: ProductWithPrice[];
